3e5fdfec615eadf8b8272fedd65cdd345c68e082
hameed9988 <44047332+hameed9988@users.noreply.github.com>
2022-03-10 15:56:47 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// api url
+const api_url = "https://hamid-news-api.herokuapp.com/news";
+const api_url2 = "http://127.0.0.1:8000/src";
+
+// Defining async function
+async function getapi(url) {
+  // Storing response
+  const response = await fetch(url);
+
+  // Storing data in form of JSON
+  var data = await response.json();
+  //console.log(data);
+  show(data);
+}
+// Calling that async function
+getapi(api_url, api_url2);
+// Function to define innerHTML for HTML table
+function show(data) {
+  let tab = ``;
+
+  for (let r of data) {
+    tab += ` <div class="container">
+      <div class="card">
+        <div class="card__header">
+          <img
+            src="${r.url}"
+            alt="${r.url}"
+            class="card__image"
+            width="600"
+          />
+        </div>
+        <div class="card__body">
+          <span class="tag tag-blue">${r.source}</span>
+          <h3> ${r.title}</h3>
+        </div>
+
+      </div>
+    </div>`;
+  }
+
+  // Setting innerHTML as tab variable
+  document.getElementById("employees").innerHTML = tab;
+}
